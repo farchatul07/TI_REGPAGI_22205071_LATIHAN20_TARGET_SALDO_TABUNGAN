f4679fb3e7e233ce0f8ae005dfc2b09e3469a2d5
farchatul07 <145920038+farchatul07@users.noreply.github.com>
2023-10-26 17:49:19 +0700

    Add files via upload

---
+++ src/main/java/com/mycompany/latihan20/Latihan20.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.latihan20;
+import java.util.Scanner;
+/**
+ *
+ * @author user
+ */
+public class Latihan20 {
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Input saldo awal, bunga per bulan, dan saldo target
+        System.out.print("Saldo awal: Rp. ");
+        double saldoAwal = input.nextDouble();
+        System.out.print("Bunga/bulan (%): ");
+        double bungaPerBulan = input.nextDouble() / 100; // Konversi persentase ke desimal
+        System.out.print("Saldo target: Rp. ");
+        double saldoTarget = input.nextDouble();
+
+
+        int bulan = 0;
+
+        while (saldoAwal < saldoTarget) {
+            saldoAwal += saldoAwal * bungaPerBulan;
+            bulan++;
+        }
+
+        System.out.println("Dalam " + bulan + " bulan, saldo Anda akan mencapai atau melebihi Rp. " + saldoTarget);
+        }
+    }
