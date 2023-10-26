6fa815baabf84c24ad11d7dc4de6f2cfd8c625b1
farchatul07 <145920038+farchatul07@users.noreply.github.com>
2023-10-26 18:24:31 +0700

    Update Latihan20.java

--- src/main/java/com/mycompany/latihan20/Latihan20.java
+++ src/main/java/com/mycompany/latihan20/Latihan20.java
@@ -2,8 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.latihan20;
+package com.mycompany.latihan20_2;
 import java.util.Scanner;
+        
 /**
  *
  * @author user
@@ -11,29 +12,32 @@ import java.util.Scanner;
  Nim               : 22205071
  Prodi             : Teknik Informatika
  Kelas             : PBO1
- Deskripsi Program : Program ini berisi program untuk Menampilkan target saldo tabungan 
+ Deskripsi Program : Program ini berisi program yang menampilkan target saldo tabungan 
  */
-public class Latihan20 {
-    
+public class Latihan20_2 {
+
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-
-        // Input saldo awal, bunga per bulan, dan saldo target
-        System.out.print("Saldo awal: Rp. ");
-        double saldoAwal = input.nextDouble();
-        System.out.print("Bunga/bulan (%): ");
-        double bungaPerBulan = input.nextDouble() / 100; // Konversi persentase ke desimal
-        System.out.print("Saldo target: Rp. ");
-        double saldoTarget = input.nextDouble();
-
-
+        Scanner scanner = new Scanner(System.in);
         int bulan = 0;
+        
+        System.out.print("Saldo Awal: ");
+        int saldoAwal = scanner.nextInt();
 
+        System.out.print("Bunga/Bulan (%): ");
+        int bungaPerBulan = scanner.nextInt();
+
+        System.out.print("saldo target: ");
+        int saldoTarget = scanner.nextInt();
+
+        
         while (saldoAwal < saldoTarget) {
-            saldoAwal += saldoAwal * bungaPerBulan;
             bulan++;
+            int bunga = saldoAwal * bungaPerBulan / 100 ;
+            saldoAwal += bunga;
+            System.out.println("Saldo di bulan ke-" + bulan + "= Rp." + saldoAwal);
+            
         }
-
-        System.out.println("Dalam " + bulan + " bulan, saldo Anda akan mencapai atau melebihi Rp. " + saldoTarget);
+    }
+}
         }
     }
